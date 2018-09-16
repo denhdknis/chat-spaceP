79bff3a0cd2d2494a33b8ec24fb7e3feb987a79d
田　英毅 <denhdk1997@yahoo.co.jp>
2018-09-16 16:12:23 +0900

    最新メッセージ表示機能の追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,18 @@ class Group < ApplicationRecord
   accepts_nested_attributes_for :members
 
   validates :name, presence: true
+
+  def last_message
+  	if (last_message = messages.last).present?
+      if last_message.body?
+       last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+    	'まだメッセージが投稿されていません'
+    end
+  end
+
+
 end
